a046e5505e1daa0280ea41ceabf377477c22ed2d
tofutigerz <hfu3@ualberta.ca>
2022-03-03 14:12:46 -0700

    makes scroll top button not focus

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-  // console.log("ready");
+  //When contents of compose tweet text-box changes, counter updates
+  //counter-error class changes color to red
   $("#tweet-text").on("input", function(e) {
-    // console.log(this);
-    // console.log($(this).val());
     const $counter = $(this).parent().find(".counter");
-    // console.log(counter.innerHTML);
-    // counter.innerHTML = "300";
-    // console.log($(counter).val(130));
     $counter.val(140 - this.value.length);
+    
     if ($counter.val() < 0) {
       $counter.addClass("counter-error");
     } else {
--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,57 +1,46 @@
 
 $(document).ready(function() {
   const $scrollButton = $("#scroll-top");
-  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  // alert(vw);
-  console.log("vh", vh);
+  const viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  
   $(document).on("scroll", function() {
-    console.log("scroll", $(window).scrollTop());
-    if ($(window).scrollTop() > 0.10 * vh) {
-      // alert("you have scrolled");
+    if ($(window).scrollTop() > 0.10 * viewportHeight) {
       $scrollButton.show();
       $(".right-nav").hide();
     } else {
       $scrollButton.hide();
-      if (vw > 390) {
-        // console.log("vw", vw);
+      if (viewportWidth > 390) {
         $(".right-nav").show();
       }
 
     }
   });
   const scrollTop = () => {
-    // const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    // console.log("scrollheight" , document.body.scrollHeight)
-    const scrollHeight = document.body.scrollHeight;
-    if (vw > 390 || scrollHeight < 2 * vh) {
-      $(window).scrollTop(0);
-      $("#tweet-text").focus();
-    } else {
-      $(window).scrollTop(vh * 2);
-      $("#tweet-text").focus();
-    }
+    // const scrollHeight = document.body.scrollHeight;
+    // if (viewportWidth > 390 || scrollHeight < 2 * viewportHeight) {
+    //   $(window).scrollTop(0);
+    //   $("#tweet-text").focus();
+    // } else {
+    //   $(window).scrollTop(viewportHeight * 2);
+    //   $("#tweet-text").focus();
+    // }
+    $(window).scrollTop(0);
+  }
+
+  const compose = () => {
+    // $(window).scrollTop(0);
+    $("#tweet-text").focus();
+    // const scrollHeight = document.body.scrollHeight;
+    // if (viewportWidth > 390 || scrollHeight < 2 * viewportHeight) {
+    //   $(window).scrollTop(0);
+    //   $("#tweet-text").focus();
+    // } else {
+    //   $(window).scrollTop(viewportHeight * 2);
+    //   $("#tweet-text").focus();
+    // }
   }
   $scrollButton.on("click", scrollTop);
-  $(".right-nav").on("click", scrollTop);
+  $(".right-nav").on("click", compose);
 });
 
-//Get the button:
-// mybutton = document.getElementById("#scroll-top");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
